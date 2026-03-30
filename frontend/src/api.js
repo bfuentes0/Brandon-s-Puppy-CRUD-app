1ecc5cf452a30c36da37e693a6f4da91dfc8d869
bfuentes0 <brandonfuentesc@utexas.edu>
2026-03-29 22:57:46 -0500

    updated frontend 5

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://brandon-s-puppy-crud-app-1.onrender.com",
+  baseURL: "https://brandon-s-puppy-crud-app.onrender.com",
 });
 
 export const setAuthToken = (token) => {
